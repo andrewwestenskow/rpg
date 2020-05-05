efb6d6f5cce8beac8f209991a6e4c5e646f04f5c
Andrew Westenskow <andrew.westenskow@gmail.com>
2020-05-05 15:46:03 -0600

    Styled party bar

--- src/components/PartyStats/index.js
+++ src/components/PartyStats/index.js
@@ -6,9 +6,19 @@ const PartyStats = ({ party }) => {
     <div className="party-stats">
       {party.map((e) => {
         const { props } = e
+        const remainingHp = (props.hp / props.maxHp) * 100
         return (
           <div key={props.id} className="stat-row">
-            <p>{props.name}</p>
+            <p className="stat-menu-name">{props.name}</p>
+            <div className="stat-menu-hp-bar">
+              <div
+                style={{ width: `${remainingHp}%` }}
+                className="stat-menu-hp-bar-inner"
+              ></div>
+            </div>
+            <p className="stat-menu-hp">
+              {props.hp}/{props.maxHp}
+            </p>
           </div>
         )
       })}
